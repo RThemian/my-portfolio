bfe186d59cecc35c4d49e8b6cb5ba8a88a3a592d
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-05-03 19:16:29 -0600

    adds styles to Header

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,14 @@
 import Image from "next/image"
+import styles from "@/styles/Header.module.css"
+import { Autour_One } from "next/font/google"
 
 function Header() {
   return (
-    <header>
-        <h1>I'm Tomas Paul</h1>
+    <header className={`${styles.header} flex-ctr-centered `}>
+        <div className="flex-ctr-centered">
+            <Image src="/Big Beard Hawaiin Shirt.JPG" width={200} height={200} alt="Tomas Paul" />
+        <h1>Hello there! I'm Tomas Paul</h1>
+        </div>
     </header>
   )
 }
